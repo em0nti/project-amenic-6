b05ab98c07bf879e3756c408fb7af5bb2bf702df
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-05-05 09:41:40 +0300

    api-fetch-service ver 1.3

--- src/js/api_fetch_service.js
+++ src/js/api_fetch_service.js
@@ -4,47 +4,42 @@ export default class ApiFetchService {
   constructor() {
     this.url = 'https://api.themoviedb.org/3/';
     this.key = '91ae85947dca7203ec2b4d7841a3c73b';
-    // this.film = 'King';
-    this.year = 2001;
-    this.genre = 'Action';
-    this.country = 'US';
+    // film release year
+    this.year = 1;
+    // film genre for filter, you can use string divides by comma ','. Example '28' get id of genres by method 'fetchFilmGenres'
+    this.genre = '';
+    //country short name for filter. Example 'US'
+    this.country = '';
     this.page = 1;
-    this.region = 'US';
     this.language = 'en-US';
-    this.id = 157336;
+    // for search by ID
+    this.id = 808;
+    // use string query for search Film
     this.query = '';
-    this.trends_type = 'day';
+    // use 'day' or 'week' to see trending list for the day or trending list for the week
+    this.trends_type = '';
   }
 
   async fetchFilmDiscoverWithCountry() {
     const urlWithParams = `${this.url}discover/movie?api_key=${this.key}&language=${this.language}&with_genres=${this.genre}&primary_release_year=${this.year}`;
-
+    console.log(urlWithParams);
     try {
       let { data } = await axios.get(urlWithParams);
-      // const dataWithOutCountry = await this.fetchFilmDiscoverWithoutCountry();
-      // console.log(dataWithOutCountry);
-      //   const arrayFilmsID = await dataWithOutCountry.map(film => film.id);
       const arrayFilmsID = await data.results.map(film => film.id);
-      //   console.log(arrayFilmsID);
-      // console.log(arrayFilmsID.length);
       let ArrayFilmID = [];
+
       for (let i = 0; i < arrayFilmsID.length; i++) {
-        const element = arrayFilmsID[i];
-        let objectByFilmID = await this.fetchFilmByID(element);
-        //   console.dir(objectByFilmID.production_countries);
-        //   console.log('dedew', Object.values(xxx.production_countries));
-        //   ArrayFilmID.[element] = xxx;
+        let objectByFilmID = await this.fetchFilmByID(arrayFilmsID[i]);
         let someArray = objectByFilmID.production_countries.some(
           country => country.iso_3166_1 === this.country,
         );
-        //   console.log('somearray', someArray);
 
         if (someArray) {
           ArrayFilmID.push(objectByFilmID);
         }
       }
 
-      //   console.log(ArrayFilmID);
+      console.log("log in method 'fetchFilmDiscoverWithCountry': ", ArrayFilmID);
       return ArrayFilmID;
     } catch (error) {
       console.log(error.message);
@@ -54,8 +49,7 @@ export default class ApiFetchService {
     try {
       const urlByID = `${this.url}movie/${filmID}?api_key=${this.key}&language=${this.language}`;
       let { data } = await axios.get(urlByID);
-      // console.log(data.production_countries);
-      // console.log(data);
+      //console.log("log in method 'fetchFilmByID': ", data);
 
       return data;
     } catch (error) {
@@ -66,7 +60,7 @@ export default class ApiFetchService {
     const urlByQuery = `${this.url}search/movie?api_key=${this.key}&language=${this.language}&query=${this.query}&page=${this.page}`;
     try {
       let { data } = await axios.get(urlByQuery);
-      console.log(data.results);
+      console.log("log in method 'fetchFilmByQuery': ", data.results);
 
       return data.results;
     } catch (error) {
@@ -78,7 +72,7 @@ export default class ApiFetchService {
     try {
       let { data } = await axios.get(urlTrends);
       // console.log(data.production_countries);
-      console.log(data);
+      console.log("log in method 'fetchFilmTrends': ", data);
 
       return data.results;
     } catch (error) {
@@ -90,9 +84,33 @@ export default class ApiFetchService {
     try {
       let { data } = await axios.get(urlUpcoming);
       // console.log(data.production_countries);
-      console.log(data.results);
+      console.log("log in method 'fetchFilmUpcoming': ", data.results);
 
       return data.results;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  async fetchFilmGenres() {
+    const urlGengres = `${this.url}genre/movie/list?api_key=${this.key}`;
+    try {
+      let { data } = await axios.get(urlGengres);
+
+      console.log("log in method 'fetchFilmGenres': ", data.genres);
+
+      return data.genres;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  async fetchFilmCountries() {
+    const urlCountries = `${this.url}configuration/countries?api_key=${this.key}`;
+    try {
+      let { data } = await axios.get(urlCountries);
+
+      console.log("log in method 'fetchFilmCountries': ", data);
+
+      return data;
     } catch (error) {
       console.log(error.message);
     }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
-import ApiFetchService from './apiFetchService';
+//this is class for fetching data
+import ApiFetchService from './api_fetch_service';
 
+// create instance 'apiFetchService' for using in functions
 const apiFetchService = new ApiFetchService();
 
-apiFetchService.setQuery = 'cat';
-apiFetchService.fetchFilmByQuery();
+// test params for fetching data by query
+//--------------//
+// set query by string see example below
+// apiFetchService.setQuery = 'cat';
+// this call return Promise use 'then' to see data;
+// apiFetchService.fetchFilmByQuery();
+apiFetchService.setCountry = 'US';
+apiFetchService.setYear = 2021;
+apiFetchService.fetchFilmGenres();
+apiFetchService.fetchFilmDiscoverWithCountry();
+apiFetchService.fetchFilmByID(apiFetchService.getID);
