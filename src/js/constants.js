e762745d41fe3fea575f3b3848bf575deead8b63
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-06 14:06:58 +0300

    add init open close js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,8 @@
 export const refs = {
-        addToLibraryBtn: document.getElementById('add-to-library-btn'),
-        remindMeBtn: document.getElementById('remind-btn'),
-}
+  addToLibraryBtn: document.getElementById('add-to-library-btn'),
+  remindMeBtn: document.getElementById('remind-btn'),
+  //modals
+  // example
+  // modalTrailer: document.querySelector('#modal-id'),
+  // modalBtn: document.querySelector('#modal-btn-id'),
+};
