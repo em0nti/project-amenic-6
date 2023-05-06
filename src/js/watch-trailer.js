da3fc12e270a7b92476efafcd601279eb3bb8893
Anton K <akarpenko83@gmail.com>
2023-05-06 15:20:23 +0300

    almost done trailer modal
    практично доробив модалку з трейлером
    не рацює - зупинка відео після закриття модалки та динамічні трейлери фільмів

--- src/js/watch-trailer.js
+++ src/js/watch-trailer.js
@@ -1,38 +1,46 @@
 
 const watchTrailerBtn = document.getElementById('watch-trailer-btn');
-const backdropToggle = document.querySelector('.backdrop--hidden');
+const modalTrailerNotOk = document.getElementById('trailer-not-ok');
+const modalTrailerOk = document.getElementById('trailer-ok');
+const trailerSrc = document.querySelector('.trailer-video');
 
 watchTrailerBtn.addEventListener('click', onWatchTrailerClick);
 
-function onWatchTrailerClick() {
-    console.log("object");
-        fetch('https://api.themoviedb.org/3/movie/809787/videos?api_key=91ae85947dca7203ec2b4d7841a3c73b&language=en-US')
+async function onWatchTrailerClick() {
+        await fetch('https://api.themoviedb.org/3/movie/640146/videos?api_key=91ae85947dca7203ec2b4d7841a3c73b&language=en-US')
         .then(response => {
         if (!response.ok) {
          throw new Error(response.status);
-            }
+        }
         return response.json();  
         })
         .then(data => {
         // Извлекаем трейлер в формате YouTube
-            console.log(data.results);
         const trailer = data.results.find(video => video.type === "Trailer" && video.site === "YouTube");
         if (trailer) {
-        const trailerUrl = `https://www.youtube.com/watch?v=${trailer.key}`;
-            console.log(trailerUrl);
-            
-        } else {
-        console.log("Трейлер не найден");
-        }
-    })
-        .catch(error => {
-            console.log(error);
-            backdropToggle.classList.remove("backdrop--hidden")
-            const closeButton = document.querySelector('.modal__cloze-button');
+        console.log(trailer.key);
+        trailerSrc.scr = `https://www.youtube.com/embed/${trailer.key}`;
+        console.log(trailerSrc.src);
+            modalTrailerOk.classList.remove("backdrop--hidden")
+            const closeButton = document.querySelector('.modal-trailer-success .modal__cloze-button');
             closeButton.addEventListener('click', closeModalClick);
             function closeModalClick() {
                 console.log("closeModalClick");
-                backdropToggle.classList.add("backdrop--hidden"); 
+                modalTrailerOk.classList.add("backdrop--hidden"); 
+            }
+        } 
+    })
+        .catch(error => {
+            console.log(error);
+            modalTrailerNotOk.classList.remove("backdrop--hidden")
+            const closeButton = document.querySelector('.modal-trailer-oops .modal__cloze-button');
+            closeButton.addEventListener('click', closeModalClick);
+            function closeModalClick() {
+                console.log("closeModalClick");
+                modalTrailerNotOk.classList.add("backdrop--hidden"); 
             }
         });
 }
+
+
+
