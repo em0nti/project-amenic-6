33a33fc0ea0cc97b6df3b29b43090d587c71dd59
Anton K <akarpenko83@gmail.com>
2023-05-06 16:50:30 +0300

    add dymanic render of iframe
    добавил динамический рендеринг iframe

--- src/js/watch-trailer.js
+++ src/js/watch-trailer.js
@@ -3,6 +3,7 @@ const watchTrailerBtn = document.getElementById('watch-trailer-btn');
 const modalTrailerNotOk = document.getElementById('trailer-not-ok');
 const modalTrailerOk = document.getElementById('trailer-ok');
 const trailerSrc = document.querySelector('.trailer-video');
+const modalIframeRef = document.querySelector('.iframe');
 
 watchTrailerBtn.addEventListener('click', onWatchTrailerClick);
 
@@ -19,9 +20,8 @@ async function onWatchTrailerClick() {
         const trailer = data.results.find(video => video.type === "Trailer" && video.site === "YouTube");
         if (trailer) {
         console.log(trailer.key);
-        trailerSrc.scr = `https://www.youtube.com/embed/${trailer.key}`;
-        console.log(trailerSrc.src);
-            modalTrailerOk.classList.remove("backdrop--hidden")
+            modalTrailerOk.classList.remove("backdrop--hidden");
+            modalIframeRef.insertAdjacentHTML('beforeend', createMarkup(trailer.key));
             const closeButton = document.querySelector('.modal-trailer-success .modal__cloze-button');
             closeButton.addEventListener('click', closeModalClick);
             function closeModalClick() {
@@ -42,5 +42,55 @@ async function onWatchTrailerClick() {
         });
 }
 
+function createMarkup(trailerKey) {
+        return `
+            <iframe
+                class="trailer-video"
+                src="https://www.youtube.com/embed/${trailerKey}">
+                allowfullscreen
+            ></iframe> 
+        `;
+}
+
+    //   // 2. This code loads the IFrame Player API code asynchronously.
+    //   const tag = document.createElement('script');
+
+    //   tag.src = "https://www.youtube.com/iframe_api";
+    //   const firstScriptTag = document.getElementsByTagName('script')[0];
+    //   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    //   // 3. This function creates an <iframe> (and YouTube player)
+    //   //    after the API code downloads.
+    //   let player;
+    //   function onYouTubeIframeAPIReady() {
+    //     player = new YT.Player('player', {
+    //       height: '360',
+    //       width: '640',
+    //       videoId: 'M7lc1UVf-VE',
+    //       events: {
+    //         'onReady': onPlayerReady,
+    //         'onStateChange': onPlayerStateChange
+    //       }
+    //     });
+    //   }
+
+    //   // 4. The API will call this function when the video player is ready.
+    //   function onPlayerReady(event) {
+    //     event.target.playVideo();
+    //   }
+
+    //   // 5. The API calls this function when the player's state changes.
+    //   //    The function indicates that when playing a video (state=1),
+    //   //    the player should play for six seconds and then stop.
+    //   let done = false;
+    //   function onPlayerStateChange(event) {
+    //     if (event.data == YT.PlayerState.PLAYING && !done) {
+    //       setTimeout(stopVideo, 6000);
+    //       done = true;
+    //     }
+    //   }
+    //   function stopVideo() {
+    //     player.stopVideo();
+    //   }
 
 
