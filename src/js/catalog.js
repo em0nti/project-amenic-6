ef847962d486c9c6df7b2901c546b6d8d97763bb
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-07 20:52:42 +0300

    first step to choices styling

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,11 +1,53 @@
 import Choices from 'choices.js';
 
 const searchRefs = {
-  input: document.querySelector('#input'),
-  year: document.querySelector('#select-year'),
-  genre: document.getElementById('#select-genre'),
-  country: document.querySelector('#select-country'),
+  input: document.querySelector('[data-input]'),
+  year: document.querySelector('[data-year]'),
+  genre: document.querySelector('[data-genre]'),
+  country: document.querySelector('[data-country]'),
 };
 
-const choisesInput = new Choices(searchRefs.input);
-const choicesYear = new Choices(searchRefs.year);
+const choicesOptions1 = {
+  searchEnabled: false,
+  shouldSort: false,
+  classNames: {
+    containerOuter: 'choices choices-input',
+    // addItems: false,
+  },
+  editItems: true,
+};
+
+const choicesOptions2 = {
+  searchEnabled: false,
+  shouldSort: false,
+  itemSelectText: '',
+  classNames: {
+    containerOuter: 'choices choices-year',
+    // addItems: false,
+  },
+};
+
+const choicesOptions3 = {
+  searchEnabled: false,
+  shouldSort: true,
+  itemSelectText: '',
+  classNames: {
+    containerOuter: 'choices choices-genre',
+  },
+};
+
+const choicesOptions4 = {
+  searchEnabled: false,
+  shouldSort: false,
+  itemSelectText: '',
+  classNames: {
+    containerOuter: 'choices choices-country',
+  },
+};
+
+// const choisesInput = new Choices(searchRefs.input, choicesOptions1);
+const choicesYear = new Choices(searchRefs.year, choicesOptions2);
+const choicesGenre = new Choices(searchRefs.genre, choicesOptions3);
+const choicesCountry = new Choices(searchRefs.country, choicesOptions4);
+
+//Create multiple choises fron searchRefs
