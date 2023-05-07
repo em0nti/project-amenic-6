79913176e87c7ae64bca9d043b057375db2599b6
Anton K <akarpenko83@gmail.com>
2023-05-07 16:21:49 +0300

    Update rating.js
    добавил код для рендера звезд рейтинга

--- src/js/rating.js
+++ src/js/rating.js
@@ -1,5 +1,39 @@
-async function getRating() {
+// в функцию надо передать рейтинг из бекнеда в виде дробного числа
+function renderRating(backendRating) {
 
- }
-
-function renderRating(rating) { }
+    const starRating = backendRating / 2; 
+    const ratingArr = [];
+    for (let i = 1; i <= 5; i++) {
+        if (i <= starRating) {
+            ratingArr.push(
+                `<li class="card__rate--item">
+                    <img
+                        class="card__rate--img"
+                        src="./images/star-full.svg"
+                        alt="star"
+                    />
+                </li>`)
+        } else if (i === Math.ceil(starRating) && starRating % 1 !== 0) {
+            ratingArr.push(
+                `<li class="card__rate--item">
+                    <img
+                        class="card__rate--img"
+                        src="./images/star-half.svg"
+                        alt="star"
+                    />
+                </li>`)
+        } else if (i > starRating) {
+            ratingArr.push(
+                `<li class="card__rate--item">
+                    <img
+                        class="card__rate--img"
+                        src="./images/star-empty.svg"
+                        alt="star"
+                    />
+                </li>`)
+        
+        }
+    }
+    return ratingArr.join(" ");
+}
+ console.log(renderRating(7));
