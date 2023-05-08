4da398d4b1001c09a5ab38c109f34627142b5039
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-08 10:24:02 +0300

    modify handler mwatch trailer

--- src/js/modals/open-close-modals.js
+++ src/js/modals/open-close-modals.js
@@ -1,15 +1,37 @@
 import { refs } from '../constants.js';
+import { supportsPassiveEvents } from 'detect-it';
 
 export function openModal(modal, player = null) {
   modal.classList.remove('backdrop--hidden');
   if (player) {
-    document.addEventListener('keydown', event => handleKeyDown(event, modal, player));
-    modal.addEventListener('click', event => handleOverlayClick(event, modal, player));
-    modal.querySelector('[data-modal-close]').addEventListener('click', () => closeModal(modal, player));
+    document.addEventListener(
+      'keydown',
+      event => handleKeyDown(event, modal, player),
+      supportsPassiveEvents ? { passive: true } : false
+    );
+    modal.addEventListener(
+      'click',
+      event => handleOverlayClick(event, modal, player),
+      supportsPassiveEvents ? { passive: true } : false
+    );
+    modal
+      .querySelector('[data-modal-close]')
+      .addEventListener('click', () => closeModal(modal, player), supportsPassiveEvents ? { passive: true } : false);
+  } else {
+    document.addEventListener(
+      'keydown',
+      event => handleKeyDown(event, modal),
+      supportsPassiveEvents ? { passive: true } : false
+    );
+    modal.addEventListener(
+      'click',
+      event => handleOverlayClick(event, modal),
+      supportsPassiveEvents ? { passive: true } : false
+    );
+    modal
+      .querySelector('[data-modal-close]')
+      .addEventListener('click', () => closeModal(modal), supportsPassiveEvents ? { passive: true } : false);
   }
-  document.addEventListener('keydown', event => handleKeyDown(event, modal));
-  modal.addEventListener('click', event => handleOverlayClick(event, modal));
-  modal.querySelector('[data-modal-close]').addEventListener('click', () => closeModal(modal));
   console.log(`Modal <${modal}> is open. Player is ${player}`);
 }
 
@@ -29,19 +51,17 @@ export function closeModal(modal, player = null) {
 }
 
 function handleKeyDown(event, modal, player = null) {
-  if (event.key === 'Escape') {
-    closeModal(modal);
-  }
   if (event.key === 'Escape' && player) {
     closeModal(modal, player);
+  } else {
+    closeModal(modal);
   }
 }
 
 function handleOverlayClick(event, modal, player = null) {
-  if (event.target === modal) {
-    closeModal(modal);
-  }
   if (event.target === modal && player) {
     closeModal(modal, player);
+  } else {
+    closeModal(modal);
   }
 }
