0dc59239c39909b9cc94112facad5b4ff66a47c7
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-09 19:18:29 +0300

    bug fix

--- src/js/modals/init-modals.js
+++ src/js/modals/init-modals.js
@@ -1,28 +1,21 @@
-import { refs } from '../constants.js';
+import { refs } from '../constants';
 import { openModal } from './open-close-modals';
 import { onWatchTrailerClick } from '../watch-trailer';
+import { onCardClick } from '../card-handler';
 
-async function initModals() {
-  try {
-    // add an event listener to open the modals
-    //example
-    //refs.watchTrailerBtn.addEventListener('click', () => openModal(refs.modalTrailer));
-    // refs.popupBtn.addEventListener('click', () => openModal(refs.modalPopup));
+export function initModals() {
+  // add an event listener to open the modals
+  //example
+  //refs.watchTrailerBtn.addEventListener('click', () => openModal(refs.modalTrailer));
+  // refs.popupBtn.addEventListener('click', () => openModal(refs.modalPopup));
 
   const currentPage = window.location.pathname;
   if (currentPage === '/library.html') {
     refs.cards.addEventListener('click', () => openModal(refs.modalPopUp));
+    refs.watchTrailerBtn.addEventListener('click', () => onWatchTrailerClick(493529));
     return;
   }
-
   refs.watchTrailerBtn.addEventListener('click', () => onWatchTrailerClick(493529));
+  refs.cards.addEventListener('click', e => onCardClick(e));
   refs.cards.addEventListener('click', () => openModal(refs.modalPopUp));
-
-
-    
-  } catch (error) {
-    console.log(error);
-  }
 }
-
-export default initModals;
