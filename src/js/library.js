5f125945565f925da912d73db42cb3823f7a8a9a
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-09 19:53:41 +0300

    bug fix

--- src/js/library.js
+++ src/js/library.js
@@ -1,10 +1,10 @@
 import { currentPage } from './show-current-page';
 import { refs } from './constants';
 import { onCardClick } from './card-handler';
-import initModals from './modals/init-modals';
+import { initModals } from './modals/init-modals';
 
 initModals();
 
 currentPage();
 
-refs.cards.addEventListener('click', onCardClick);
+onCardClick();
