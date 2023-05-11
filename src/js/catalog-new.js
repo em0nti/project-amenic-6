fb2576939b02ecb4df1f93d0f84254b3ed8e631d
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-05-11 20:28:20 +0300

    weekly trends beta

--- src/js/catalog-new.js
+++ src/js/catalog-new.js
@@ -1,10 +1,9 @@
-
-import { tmdbApi } from './components/tmdbApi';
+import { tmdbApi, trendsApi } from './components/tmdbApi';
 
 import scrollToTop from './components/scroll-to-top';
-import intersection from './components/infinite-scroll'
+import intersection from './components/infinite-scroll';
 import appendMovieCards from './components/append-movie-cards';
-import clearPage from "./components/clear-page";
+import clearPage from './components/clear-page';
 // import initChoices from './search-styling';
 import ApiFetchService from './api_fetch_service';
 import ApiMarkupService from './api_markup_service';
@@ -30,13 +29,12 @@ import { openModal } from './modals/open-close-modals';
 
 currentPage();
 
+const bodyRef = document.querySelector('body');
 
-const bodyRef = document.querySelector('body'),
+const inputRef = document.querySelector('input');
+const cardSetRef = document.querySelector('.card-set');
 
-const inputRef = document.querySelector('input'),
-const cardSetRef = document.querySelector('.card-set'),
-
-const formRef = document.querySelector('.search-form'),
+const formRef = document.querySelector('.search-form');
 
 refs.cards.addEventListener('click', e => onCardClick(e));
 refs.cards.addEventListener('click', () => openModal(refs.modalPopUp));
@@ -44,24 +42,46 @@ refs.cards.addEventListener('click', () => openModal(refs.modalPopUp));
 formRef.addEventListener('submit', onSearch);
 
 function onSearch(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    tmdbApi.form = formRef;
-    tmdbApi.query = event.currentTarget.elements.query.value.trim();
+  tmdbApi.form = formRef;
+  tmdbApi.query = event.currentTarget.elements.query.value.trim();
 
-    handleSearch(tmdbApi.query);
-}; 
+  handleSearch(tmdbApi.query);
+}
 async function handleSearch() {
-    try {
-    if (tmdbApi.query === "") {
-    throw new Error()
+  try {
+    if (tmdbApi.query === '') {
+      throw new Error();
     }
     clearPage();
-    await appendMovieCards()    
+    await appendMovieCards();
     scrollToTop();
-    intersection()    
-    } catch (error) {
-        console.log(error.message);
-        
-    }
+    intersection();
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function showWeeklyTrends() {
+  try {
+    // if (tmdbApi.query === '') {
+    //   throw new Error();
+    // }
+    clearPage();
+    await appendMovieCards();
+    scrollToTop();
+    intersection();
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function onWeeklyTrends() {
+  //   event.preventDefault();
+
+  //   trendsApi.form = formRef;
+  trendsApi.trendsType = 'week';
+
+  showWeeklyTrends();
 }
---
+++ src/js/components/TrendsApi.js
@@ -0,0 +1,2 @@
+import TrendsApi from '../api_tmdb/api_fetch-weekly-trends';
+export const trendsApi = new TrendsApi();
