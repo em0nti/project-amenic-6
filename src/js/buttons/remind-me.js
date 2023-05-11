d6c035718afd4211f9dd7573b7010bd0001182fa
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-05-11 12:07:42 +0300

    Update remind-me.js

--- src/js/buttons/remind-me.js
+++ src/js/buttons/remind-me.js
@@ -21,30 +21,30 @@ let id = state.activeCard.id;
 export function onClickAddToLibrary(ID) {
   console.log('Our ID: ', ID);
   addIdArrToStorage(ID);
-}
 
-// ф-я яка завантажує дані в локал сторадж
+  // ф-я яка завантажує дані в локал сторадж
 
-function addIdArrToStorage(id) {
-  const storageValue = load(STORAGE_KEY);
-  console.log(storageValue);
-  if (storageValue === undefined) {
-    // створюю масив і додаю перший єл-т і додаю в локал
-    const idArr = [id];
-    save(STORAGE_KEY, idArr);
-  } else if (verifycontainsId(storageValue, id)) {
-    //первіряємо якщо id вже є повідомлення і виходимо
-    Notiflix.Notify.info('You alredy have this movie in the library');
-    // console.log("вже маємо")
-  } else {
-    // до вже існуючго масиву додаємо значення
-    storageValue.push(id);
-    save(STORAGE_KEY, storageValue);
+  function addIdArrToStorage(id) {
+    const storageValue = load(STORAGE_KEY);
+    console.log(storageValue);
+    if (storageValue === undefined) {
+      // створюю масив і додаю перший єл-т і додаю в локал
+      const idArr = [id];
+      save(STORAGE_KEY, idArr);
+    } else if (verifycontainsId(storageValue, id)) {
+      //первіряємо якщо id вже є повідомлення і виходимо
+      Notiflix.Notify.info('You alredy have this movie in the library');
+      // console.log("вже маємо")
+    } else {
+      // до вже існуючго масиву додаємо значення
+      storageValue.push(id);
+      save(STORAGE_KEY, storageValue);
+    }
+
+    console.log(storageValue);
   }
 
-  console.log(storageValue);
-}
-
-function verifycontainsId(arr, x) {
-  return (hasId = arr.includes(x));
+  function verifycontainsId(arr, x) {
+    return (hasId = arr.includes(x));
+  }
 }
