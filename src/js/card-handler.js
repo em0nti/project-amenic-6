7234fbc63026b2fb37da67861247131ad761e5ab
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-11 14:09:17 +0300

    add feature Add to my library to Pop up

--- src/js/card-handler.js
+++ src/js/card-handler.js
@@ -1,5 +1,6 @@
 import { state } from './constants.js';
 import { onShowPopUpModal } from './functions.js';
+import { onClickAddToLibrary } from './add-to-library-handler';
 
 // refs.cards.addEventListener('click', onCardClick);
 export function onCardClick(e) {
@@ -7,8 +8,12 @@ export function onCardClick(e) {
   const card = e.target.closest('.card');
   const movieId = card.dataset.id;
   state.activeCard.id = movieId;
-  console.log(movieId);
-  onShowPopUpModal(movieId).then(() => {
-    document.getElementById('add-to-library-btn').addEventListener('click', () => console.log('Add to my Library Listener works'));
+  onShowPopUpModal(movieId).then((cardID) => {
+    const cardStorage = state.cardStorage;
+    if (cardStorage.hasCardId(cardID)) {
+      document.getElementById('add-to-library-btn').textContent = 'Remove from my library'
+    }
+    document.getElementById('add-to-library-btn').addEventListener('click', () => onClickAddToLibrary(cardID));
+
   });
 }
